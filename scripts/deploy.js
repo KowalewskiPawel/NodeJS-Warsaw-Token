f9dd3451282005bcb15faf0f0c8feb4b18b0e7df
KowalewskiPawel <pawelkowalewsk@gmail.com>
2022-01-04 20:57:34 +0100

    basic non-erc20 token

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+(async () => {
+  try {
+    const NodeJSToken = await ethers.getContractFactory("NodeJSToken");
+
+    const deployedToken = await NodeJSToken.deploy();
+
+    console.log("Contract deployed to address:", deployedToken.address);
+
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
